bd2d80fd24bd6a3626325017afa236e4539abc13
RoggerParedes <rparedest@fi.uba.ar>
2022-11-05 01:34:48 -0300

    Repositorio creado

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tp_clojure_tdl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tp_clojure_tdl/core_test.clj
@@ -0,0 +1,7 @@
+(ns tp-clojure-tdl.core-test
+  (:require [clojure.test :refer :all]
+            [tp-clojure-tdl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
