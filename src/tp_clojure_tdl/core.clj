6656c695cce42c9afeba93b64fecda4d81ecc989
RoggerParedes <rparedest@fi.uba.ar>
2022-11-10 03:05:05 -0300

    Se agrega la lectura de los archivos Se agrega la validacion de la extencion de los archivos Se agrega sus test

--- src/tp_clojure_tdl/core.clj
+++ src/tp_clojure_tdl/core.clj
@@ -1,7 +1,44 @@
 (ns tp-clojure-tdl.core
-  (:gen-class))
+  (:gen-class)
+)
+
+(defn validate_extension_file [name_file]
+  ( do
+    ( def extension_file 
+      (second (re-find #"(\.[a-zA-Z0-9]+)$" name_file))
+    )
+    ( if (= ".json" extension_file)
+      true
+      false
+    )
+  )  
+)
+
+(defn validate_file [doc]
+  ( if (not-empty doc)
+    true
+    false
+  )
+)
+
+(defn init_service [] 
+  ( do
+    ( loop []
+      ( do
+        (println "Ingrese la ruta del Archivo:")
+        (def document (read-line))
+        (def validate (validate_file document))
+        ( if (true? validate)
+          (println document)
+          (recur)
+        )
+      )
+    )
+  )
+)
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (init_service)
+)
+
--- test/tp_clojure_tdl/core_test.clj
+++ test/tp_clojure_tdl/core_test.clj
@@ -4,4 +4,26 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest b-test
+  (testing "If the Inut file is not empty then return true")
+  (is (= true (validate_file "data")))
+)
+
+(deftest c-test
+  (testing "If the Inut file is empty then return False")
+  (is (= false (validate_file "")))
+)
+    
+(deftest d-test
+  (testing "If we receive file with extension .json then, validate_extension_file return True"
+    (is (= true (validate_extension_file "arch.json")))
+  )
+)
+
+(deftest e-test
+  (testing "If we receive file without extension .json then, validate_extension_file return False"
+    (is (= false (validate_extension_file "arch.html")))
+  )
+)
